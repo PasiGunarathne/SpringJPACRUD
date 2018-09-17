256554fe69fa3702fdd6cc963ad502f32c6000b8
PasiGunarathne <pasigunarathne@gmail.com>
2018-09-18 01:11:43 +0530

    added model article to SpringJPACRUD

--- src/main/java/com/example/technicalTest/model/Article.java
+++ src/main/java/com/example/technicalTest/model/Article.java
@@ -1,8 +1,49 @@
 package com.example.technicalTest.model;
 
+import javax.persistence.*;
+
 /**
  * Created by pasindu on 3/7/19.
  */
+
+
+@Entity
+@Table(name = "article")
 public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "category")
+    private String category;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
 
