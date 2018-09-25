ab0e280a63deaeb5ad6946130638cc2080493bbc
PasiGunarathne <pasigunarathne@gmail.com>
2018-09-26 01:39:44 +0530

    added service ArticleService to SpringJPACRUD

---
+++ src/main/java/com/example/technicalTest/service/ArticalService.java
@@ -0,0 +1,15 @@
+package com.example.technicalTest.service;
+
+import com.example.technicalTest.model.Article;
+
+import java.util.List;
+
+/**
+ * Created by pasindu on 3/8/19.
+ */
+public interface ArticalService {
+
+    public List<Article> getAllArticles();
+
+    public Article getArticleById(Long id);
+}
